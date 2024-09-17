072e8dc5ee12f3321a0064c4f9a0cf6b1eca1006
nahom <nbirhane@gmail.com>
2024-09-17 00:03:12 -0500

    server and client side added lab-10

---
+++ lab-7/server/src/routes/BookRouter.ts
@@ -0,0 +1,9 @@
+import { Router } from "express";
+import * as BookController from '../controller/BookController'
+export const BookRouter = Router();
+
+BookRouter.post('/books', BookController.save);
+BookRouter.get('/books', BookController.getAllBooks);
+BookRouter.put('/books/:id', BookController.updateBookById);
+BookRouter.delete('/books/:id', BookController.deleteBookById);
+
